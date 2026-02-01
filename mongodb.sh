0f3732e86ff7834480029d32525fa417db3f18db
mayuri <mayu.reddy10@gmail.com>
2026-02-01 16:54:52 +1100

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -29,13 +29,13 @@ validate() {
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 validate $? "copying mongo repo"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$logs_file
 validate $? "Installing mongodb server"
 
-systemctl enable mongod
+systemctl enable mongod &>>$logs_file
 validate $? "Enable mongodb"
 
-systemctl start mongod
+systemctl start mongod &>>$logs_file
 validate $? "Start mongodb"
  
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
