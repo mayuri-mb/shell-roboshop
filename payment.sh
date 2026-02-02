9015ae43a357d29803b1c5db4e25819891df3199
mayuri <mayu.reddy10@gmail.com>
2026-02-03 00:44:13 +1100

    actions

---
+++ payment.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+user=$(id -u)
+logs_folder="/var/log/shell-roboshop"
+logs_file="$logs_folder/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+script_dir=$PWD
+mongodb_host=mongodb.daws-88s.online
+
+if [ $user -ne 0 ]; then
+   echo -e "$R Please run this script with root user access $N" | tee -a $logs_file
+   exit 1
+fi
+
+mkdir -p $logs_folder
+
+validate() {
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 $R failed $N"  | tee -a $logs_file
+       exit 1
+    else 
+       echo -e "$2 $R success $N" | tee -a $logs_file
+    fi   
+}
+
+dnf install python3 gcc python3-devel -y ?>>$logs_file
+validate $? "installing Python"
+
+id roboshop &>>$logs_file
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$logs_file
+   echo "Creating system user"
+else
+   echo -e "Roboshop user already exist $Y Skipping $N"
+fi
+
+mkdir -p /app  &>>$logs_file
+validate "creating app directory"
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$logs_file
+validate $? "downloading payment code"
+
+cd /app  &>>$logs_file
+validate $? "moving to app directory"
+
+unzip /tmp/payment.zip &>>$logs_file
+validate "unzip code"
+
+cd /app 
+pip3 install -r requirements.txt &>>$logs_file
+validate $? "installing dependencies"
+
+cp $script_dir/payment.service /etc/systemd/system/payment.service &>>$logs_file
+validate $? "created systemctl service"
+
+systemctl daemon-reload &>>$logs_file
+validate $? "load the service"
+
+systemctl enable payment 
+systemctl start payment &>>$logs_file
+validate $? "enabled and started paymant"
+
+
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -28,16 +28,16 @@ validate() {
     fi   
 }
 
-cp $script_dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+cp $script_dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$logs_file
 validate $? "added rabitmq repo"
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>$logs_file
 validate $? "installing rabbitmq"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>$logs_file
+systemctl start rabbitmq-server &>>$logs_file
 validate $? "enabled and started rabbitmq"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-validate $? "created users and given permissions"
+rabbitmqctl add_user roboshop roboshop123 &>>$logs_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$logs_file
+validate $? "created users and given permissions" 
