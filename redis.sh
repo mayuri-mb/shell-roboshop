e4a19bf09e45e9bf33ea752578b8bbccfaef7255
mayuri <mayu.reddy10@gmail.com>
2026-02-02 21:28:53 +1100

    actions

--- redis.sh
+++ redis.sh
@@ -36,7 +36,7 @@ validate $? "installing redis"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 validate $? "Allowing remote connections"
 
-dnf enable redis -y &>> $logs_file
-dnf start redis -y 
-validate $? "ebaled and started redis"
+systemctl enable redis -y &>> $logs_file
+systemctl start redis -y 
+validate $? "enabled and started redis"
 
