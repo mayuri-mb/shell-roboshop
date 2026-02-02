906447d94eb41848d28a8157a5cade570732af7c
mayuri <mayu.reddy10@gmail.com>
2026-02-03 00:06:39 +1100

    actions

--- shipping.sh
+++ shipping.sh
@@ -64,15 +64,17 @@ validate $? "moving and renaming shipping"
 cp $script_dir/shipping.service /etc/systemd/system/shipping.service &>>$logs_file
 validate $? "creating system user"
 
-systemctl daemon-reload 
-validate $? "loading the service"
-
 dnf install mysql -y  &>>$logs_file
 validate $? "installing mysql"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$logs_file
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$logs_file
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$logs_file
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$logs_file
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$logs_file
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$logs_file
+   validate $? "Loaded data into mysql"
+else
+   echo -e "Data is already loaded, $Y Skipping $N"
 
 
 systemctl enable shipping
