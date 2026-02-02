1a5c5b84b6fa4f0b5d32a8af6aaf85360dde3a39
mayuri <mayu.reddy10@gmail.com>
2026-02-02 19:29:43 +1100

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 user=$(id -u)
-log_folder="/var/log/shell-roboshop"
-log_file="/var/logs/$0.log"
+logs_folder="/var/log/shell-roboshop"
+logs_file="/var/logs/$0.log"
 
 R="\e[31m"
 G="\e[32m"
@@ -12,7 +12,7 @@ script_dir=$pwd
 mongodb_host=mongodb.daws-88s.online
 
 if [ $user -ne 0 ]; then
-   echo -e "$R Please run this script with root user access $N" | tee -a $log_file
+   echo -e "$R Please run this script with root user access $N" | tee -a $logs_file
    exit 1
 fi
 
@@ -20,25 +20,25 @@ mkdir -p $logs_folder
 
 validate() {
     if [ $1 -ne 0 ]; then
-       echo -e "$2 $R failed $N"  | tee -a $log_file
+       echo -e "$2 $R failed $N"  | tee -a $logs_file
        exit 1
     else 
-       echo -e "$2 $R success $N" | tee -a $log_file
+       echo -e "$2 $R success $N" | tee -a $logs_file
     fi   
 }
 
-dnf module disable nodejs -y &>> $log_file
+dnf module disable nodejs -y &>> $logs_file
 validate $? "disabling nodejs default module"
 
-dnf module enable nodejs -y &>> $log_file
+dnf module enable nodejs -y &>> $logs_file
 validate $? "enabling nodejs 20"
 
-dnf install nodejs -y &>> $log_file
+dnf install nodejs -y &>> $logs_file
 validate $? "installing nodejs"
 
 id roboshop
 if [ $? -ne 0 ]; then
-   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $log_file
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $logs_file
    validate $? "Creating system user"
 else
    echo "user already created"
@@ -47,30 +47,30 @@ fi
 mkdir -p /app 
 validate $? "creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $log_file
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $logs_file
 validate $? "downloading catalogue code"
 
 cd /app 
 validate $? "moving to app directory"
 
-unzip /tmp/catalogue.zip &>> $log_file
+unzip /tmp/catalogue.zip &>> $logs_file
 validate $? "Unzip catalogue code"
 
-npm install &>> $log_file
+npm install &>> $logs_file
 validate $? "downloading dependencies"
 
-cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service &>> $log_file
+cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service &>> $logs_file
 validate $? "Created systemctl service"
 
 systemctl daemon reload
 validate $? "Loading the service"
 
 systemctl enable catalogue
-systemctl start catalogue &>> $log_file
+systemctl start catalogue &>> $logs_file
 validate $? "enabling and starting the service"
 
 cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y &>> $log_file
+dnf install mongodb-mongosh -y &>> $logs_file
 
 index=$(mongosh --host $mongodb_host --quiet -eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
